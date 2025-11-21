f32fe49290db4319c1fe750ed0a9e7132eeb307f
Frelnix <felixdarma1235@gmail.com>
2025-11-21 18:16:21 +0700

    Update TR-DDP.c

--- TR-DDP.c
+++ TR-DDP.c
@@ -43,7 +43,68 @@ void formatTanggal(time_t waktu, char *buffer);
 void lihatDaftarBuku(); // Diperlukan oleh PinjamBuku
 
 Isi disini bagian 2
+void bersihLayar() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void tungguEnter() {
+    printf("\nTekan Enter untuk melanjutkan...");
+    while (getchar() != '\n');
+    getchar();
+}
+
+void loading() {
+    bersihLayar();
+    printf("Memverifikasi data...\n\n");
+    // Simulasi loading bar sederhana
+    printf("[");
+    for (int i = 0; i < 20; i++) {
+        printf("=");
+        Sleep(50); // Delay 50ms
+    }
+    printf("]\n");
+    printf("\nLogin Berhasil! Selamat datang.\n");
+    Sleep(1000);
+}
+
+void formatTanggal(time_t waktu, char *buffer) {
+    strftime(buffer, 20, "%Y-%m-%d", localtime(&waktu));
+}
+
+int login() {
+    char username[50], password[50];
+    int percobaan = 0;
+
+    while (percobaan < 3) {
+        bersihLayar();
+        printf("==============================\n");
+        printf("    LOGIN SISTEM PERPUSTAKAAN\n");
+        printf("==============================\n");
+        printf("Username : "); scanf("%s", username);
+        printf("Password : "); scanf("%s", password);
+
+        // Hardcoded credential
+        if (strcmp(username, "admin") == 0 && strcmp(password, "admin123") == 0) {
+            strcpy(currentUser, username);
+            loading();
+            return 1; // Login Sukses
+        } else {
+            printf("\n[!] Username atau password salah.\n");
+            percobaan++;
+            Sleep(1500);
+        }
+    }
+    printf("\n[!] Akses ditolak setelah 3x percobaan.\n");
+    return 0; // Login Gagal
+}
+
+
 Isi disini bagian 3 
 Isi disini bagian 4
+
 
 
